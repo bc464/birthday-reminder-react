fb6bd84dab6fce3ff68f2297071cac48fe475268
bc464 <fil419@webmail.co.za>
2022-09-27 16:08:24 +0200

    Updates

---
+++ precache-manifest.d4467cbb8d631c877a0ac841dd66f173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045d7764e3c1a2779cf8d70c7367dbf3",
+    "url": "/birthday-reminder-react/index.html"
+  },
+  {
+    "revision": "7a6a856103cc20ae353f",
+    "url": "/birthday-reminder-react/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder-react/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder-react/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6a856103cc20ae353f",
+    "url": "/birthday-reminder-react/static/js/main.f9b9d867.chunk.js"
+  },
+  {
+    "revision": "53fd85258c192f755c3b",
+    "url": "/birthday-reminder-react/static/js/runtime-main.9a26030c.js"
+  }
+]);
